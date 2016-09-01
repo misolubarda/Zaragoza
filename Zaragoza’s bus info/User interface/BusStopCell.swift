f3deba9df8e36f8ea154ff892a148bfda790c8af
Miso Lubarda <miso.lubarda@gmail.com>
2016-09-02 00:03:36 +0200

    table view with cells created, updating cells with bus stop images on scroll events

--- Zaragoza’s bus info/User interface/BusStopCell.swift
+++ Zaragoza’s bus info/User interface/BusStopCell.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2016 Miso Lubarda. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class BusStopCell: UITableViewCell {
+    
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var nameTextView: UITextView!
+    @IBOutlet private weak var mapImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameTextView.textContainerInset = UIEdgeInsetsZero
+    }
+    
+    var name: String? {
+        didSet {
+            nameTextView.text = name
+        }
+    }
+    var number: Int? {
+        didSet {
+            if number != nil {
+                numberLabel.text = "\(number!)"
+            } else {
+                numberLabel.text = ""
+            }
+        }
+    }
+    var imageData: NSData? {
+        didSet {
+            if let imageData = imageData {
+                mapImageView.image = UIImage(data: imageData)
+            } else {
+                mapImageView.image = nil
+            }
+        }
+    }
+}
--- Zaragoza’s bus info/User interface/MainViewController.swift
+++ Zaragoza’s bus info/User interface/MainViewController.swift
@@ -9,5 +9,85 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let busStopCellIdentifier = "BusStopCell"
+    private var busStops = [BusStop]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 150
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let request = BusStopsAPIRequest()
+        
+        request.executeWithCompletion { [weak self] (response: () throws -> [BusStop]) in
+            do {
+                self?.busStops = try response()
+                self?.tableView.reloadData()
+                self?.updateCellsWithImages()
+            } catch _ {
+                
+                // REMARK: Would use UIAlertController or preferably centralize alert handling.
+                UIAlertView(title: "Error", message: "Something went wrong", delegate: nil, cancelButtonTitle: "OK").show()
+            }
+        }
+    }
+}
 
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(busStopCellIdentifier, forIndexPath: indexPath) as! BusStopCell
+        
+        let busStop = busStops[indexPath.row]
+        
+        cell.name = busStop.name
+        cell.number = busStop.number
+        cell.imageData = busStop.mapImage
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return busStops.count
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            updateCellsWithImages()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        updateCellsWithImages()
+    }
+
+    func updateCellsWithImages() {
+        
+        debugPrint("end scrolling")
+        
+        guard let visibleIndexPaths = self.tableView.indexPathsForVisibleRows else {
+            return
+        }
+        
+        let visibleBusStops = visibleIndexPaths.map { (indexPath) -> BusStop in
+            return busStops[indexPath.row]
+        }
+        
+        for busStop in visibleBusStops {
+            busStop.getImageWithCompletion{ [weak self] (image) in
+                self?.tableView.reloadData()
+            }
+        }
+    }
 }
