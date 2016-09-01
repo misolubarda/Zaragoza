17e22c14253dfa80b2600ff30c79916cd13d269b
Miso Lubarda <miso.lubarda@gmail.com>
2016-09-01 17:26:35 +0200

    folder structure created

---
+++ Zaragoza’s bus info/User interface/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  Zaragoza’s bus info
+//
+//  Created by Miso Lubarda on 01/09/16.
+//  Copyright © 2016 Miso Lubarda. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+}
